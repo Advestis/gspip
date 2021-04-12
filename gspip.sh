53d83c187b72b9b6508e0b1b528c3012c910e36e
pcotte <pcotte@advestis.com>
2021-04-12 12:01:16 +0200

    new: usr: can call gspip.sh without CRED

--- gspip.sh
+++ gspip.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-BUCKET=pypi_server_prod
+BUCKET=""
 SKIP=""
 UPGRADE="no"
 VERSION_TO_GET=""
 PATH_TO_CRED=/media/SERVEUR/_configs/pypi_server_prod_credential.json
-CRED="-o Credentials:gs_service_key_file=${PATH_TO_CRED}"
+CRED=""
+if [ "$PATH_TO_CRED" != "" ] ; then
+  CRED="-o Credentials:gs_service_key_file=${PATH_TO_CRED}"
+fi
 
 while true; do
   case "$1" in
@@ -238,7 +241,11 @@ function install() {
 
   echo "$SKIP""Will install package $PACKAGE from $PACKAGE_LOCATION$thefile"
 
-  if ! gsutil $CRED cp "$PACKAGE_LOCATION$thefile" "/tmp/$thefile" ; then return 1 ; fi
+  if [ "$CRED" != "" ] ; then
+    if ! gsutil $CRED cp "$PACKAGE_LOCATION$thefile" "/tmp/$thefile" ; then return 1 ; fi
+  else
+    if ! gsutil cp "$PACKAGE_LOCATION$thefile" "/tmp/$thefile" ; then return 1 ; fi
+  fi
   if ! [ -f "/tmp/$thefile" ] ; then
     echo "No file downloaded!"
     return 1
@@ -276,32 +283,6 @@ function recreate_dist() {
   return 0
 }
 
-function push() {
-
-  PACKAGE=$(basename "$(pwd)" | tr '[:upper:]' '[:lower:]')
-
-  if [ -d dist ] ; then
-    thefile=$(find dist/* | grep ".tar.gz" | head -n 1)
-    if [ "$thefile" == "" ] ; then
-      echo "$SKIP""No .tar.gz in dist. Recreating..."
-      if ! recreate_dist ; then
-        exit 1
-      fi
-    fi
-  else
-    echo "$SKIP""No dist directory. Recreating..."
-    if ! recreate_dist ; then
-      exit 1
-    fi
-  fi
-
-  echo "$SKIP""Will now push $PACKAGE to gcs."
-  if ! gsutil $CRED cp "$thefile" "gs://$BUCKET/$PACKAGE/" ; then
-    exit 1
-  fi
-  if [ -d "dist" ] ; then rm -r dist ; fi
-}
-
 if [ "$COMMAND" == "install" ] ; then
   while [ "$BUCKET" == "" ] ; do
     echo "Bucket was not provided (--bucket argument was not specified, and envvar PIP_BUCKET was not found). Please provide a bucket:"
@@ -325,14 +306,6 @@ BUCKET=pypi_server_prod
   fi
 elif [ "$COMMAND" == "uninstall" ] || [ "$COMMAND" == "remove" ] ; then
   uninstall
-elif [ "$COMMAND" == "push" ]  ; then
-
-  if ! gsutil $CRED ls gs://$BUCKET/ &> /dev/null ; then
-    echo "Could not talk to gs://bucket pypi_server_sand : do you have the authorisations?"
-    exit 1
-  fi
-
-  push
 else
   echo "$SKIP""Unknown command $COMMAND"
 fi
